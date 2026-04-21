7e7d5b2e1ae19d0f6784f89b1e5e18df25a3aaa0
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-21 10:53:20 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //variables
+        int pesoMan, pesoTt, TotalMan;
+        //inicio
+        pesoMan = 0;
+        pesoTt = 0;
+        TotalMan = 0;
+        while (TotalMan < 1000) ;
+        { 
+        Console.WriteLine("ingrese el peso de la manzana:");
+        pesoMan = Convert.ToInt32(Console.ReadLine());
+            if (pesoMan > 180)
+            {
+                pesoTt += pesoMan;//Abreviada de un acumulador
+                //pesoTt=pesoTt+pesoMan
+                Console.WriteLine("llevo el total de peso" + pesoTt + "Gramos");
+
+                TotalMan++;
+            }
+            else
+            {
+                Console.WriteLine("ya alcanzo o supero 1 kilo de manzanas");
+                Console.WriteLine("Cantidad de total de manzana :" + TotalMan);
+            }
+        }
+    }
+}
